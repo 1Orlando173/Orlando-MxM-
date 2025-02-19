6344ecca0b9b23691d154429176232591af0d03b
Orlando <rc9314832@gmail.com>
2025-02-18 22:00:30 -0600

    Actualizar main-autobio.js

--- plugins/main-autobio.js
+++ plugins/main-autobio.js
@@ -14,7 +14,7 @@ process.once('message', resolve);
 setTimeout(resolve, 2000)}) * 1000;
 }
 let uptime = clockString(_uptime);
-let bio = `『${global.packname}』 |「🕒」𝐀𝐜𝐭𝐢𝐯𝐨: ${uptime} |「</>」 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝: Orlando 👑` 
+let bio = `『${global.packname}』 |「🕒」𝐀𝐜𝐭𝐢𝐯𝐨: ${uptime} |「</>」 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐝: Orlando el mas pitudo😈` 
 await this.updateProfileStatus(bio).catch(_ => _);
 setting.autobio = new Date() * 1;
 };
